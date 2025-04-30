c24571c9259ae82bdebcfc3394d9cc0e36af96bd
marlon <mstick2607@gmail.com>
2025-04-29 22:21:46 -0500

    feact: Crear  rutas para poder accder a los componentes po interface2

--- src/app/components/cryto-detail/cryto-detail.component.ts
+++ src/app/components/cryto-detail/cryto-detail.component.ts
@@ -153,7 +153,7 @@ export class CrytoDetailComponent implements OnInit {
 
         const prices = data.map((item: any) => item.price)
         const dates = data.map((item: any) => new Date(item.fetched_at).toLocaleTimeString())
-
+//
         this.lineChartData.labels = dates
         this.lineChartData.datasets[0].data = prices
 
